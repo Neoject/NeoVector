e040668e71d70d47c7162feecddab485e8e51d31
Neoject <100164615+Neoject@users.noreply.github.com>
2026-03-28 18:44:16 +0300

    исправление порядка подключения скриптов

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
         <div class="neoject">Сайт разработан <a class="btn btn-outline" style="border:none"
              href="https://neoject.by" target="_blank">neoject.by</a></div>
         <script src="<?=NV?>/main/js/sections.js"></script>
+        <script src="<?=NV?>/main/js/script.js"></script>
     </body>
 </html>
